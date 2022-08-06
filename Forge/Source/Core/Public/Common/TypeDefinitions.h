3a34c04796ca3373c4cd1856ea2340efd1c6c4ca
FudgeRacoon <FudgeRacoon@gmail.com>
2022-08-06 17:35:08 +0200

    Added compatiblity for different fixed-size data types

--- Forge/Source/Core/Public/Common/TypeDefinitions.h
+++ Forge/Source/Core/Public/Common/TypeDefinitions.h
@@ -1,15 +1,28 @@
 #ifndef TYPE_DEFINITIONS_H
 #define TYPE_DEFINITIONS_H
 
+#include "Compiler.h"
+
 namespace Forge
 {
-	/// Byte types
-	using BYTE  = unsigned __int8;
-	using CBYTE = const unsigned __int8;
+	/// Misc types
+	using VOID = void;
 
 	/// Char types
-	using CHAR  = char;
+	using CHAR = char;
 	using CCHAR = const char;
+
+	/// Floating-point types
+	using F32  = float;
+	using F64  = double;
+	using CF32 = const float;
+	using CF64 = const double;
+
+#if defined(FORGE_COMPILER_MSVC)
+	/// Misc types
+	using SIZE  = unsigned __int64;
+	using BYTE  = unsigned __int8;
+	using CBYTE = const unsigned __int8;
 
 	/// Unsigned integer types
 	using U8   = unsigned __int8;
@@ -20,7 +33,7 @@ namespace Forge
 	using CU16 = const unsigned __int16;
 	using CU32 = const unsigned __int32;
 	using CU64 = const unsigned __int64;
-	
+
 	/// Signed integer types
 	using I8   = signed __int8;
 	using I16  = signed __int16;
@@ -30,50 +43,70 @@ namespace Forge
 	using CI16 = const signed __int16;
 	using CI32 = const signed __int32;
 	using CI64 = const signed __int64;
-	
-	/// Floating-point types
-	using F32  = float;
-	using F64  = double;
-	using CF32 = const float;
-	using CF64 = const double;
+#elif
+	#include<stdint>
 
-	/// Void pointer types
-	using PVOID  = void*;
-	using PCVOID = const void*;
+	/// Misc types
+	using SIZE  = std::uint64_t;
+	using BYTE  = std::uint8_t;
+	using CBYTE = const std::uint8_t;
 
-	/// Byte pointer types
-	using PBYTE  = unsigned __int8*;
-	using PCBYTE = const unsigned __int8*;
+	/// Unsigned integer types
+	using U8   = std::uint8_t;
+	using U16  = std::uint16_t;
+	using U32  = std::uint32_t;
+	using U64  = std::uint64_t;
+	using CU8  = const std::uint8_t;
+	using CU16 = const std::uint16_t;
+	using CU32 = const std::uint32_t;
+	using CU64 = const std::uint64_t;
+
+	/// Signed integer types
+	using I8   = std::int8_t;
+	using I16  = std::int16_t;
+	using I32  = std::int32_t;
+	using I64  = std::int64_t;
+	using CI8  = const std::int8_t;
+	using CI16 = const std::int16_t;
+	using CI32 = const std::int32_t;
+	using CI64 = const std::int64_t;
+#endif
+
+	/// Misc pointer types
+	using PVOID  = VOID*;
+	using PCVOID = const VOID*;
+	using PBYTE  = BYTE*;
+	using PCBYTE = CBYTE*;
 
 	/// Char pointer types
-	using PSTR = char*;
-	using PCSTR = const char*;
+	using PSTR  = CHAR*;
+	using PCSTR = CCHAR*;
 
 	/// Unsinged integer pointer types
-	using PU8   = unsigned __int8*;
-	using PU16  = unsigned __int16*;
-	using PU32  = unsigned __int32*;
-	using PU64  = unsigned __int64*;
-	using PCU8  = const unsigned __int8*;
-	using PCU16 = const unsigned __int16*;
-	using PCU32 = const unsigned __int32*;
-	using PCU64 = const unsigned __int64*;
+	using PU8   = U8*;
+	using PU16  = U16*;
+	using PU32  = U32*;
+	using PU64  = U64*;
+	using PCU8  = CU8*;
+	using PCU16 = CU16*;
+	using PCU32 = CU32*;
+	using PCU64 = CU64*;
 
 	/// Singed integer pointer types
-	using PI8   = signed __int8*;
-	using PI16  = signed __int16*;
-	using PI32  = signed __int32*;
-	using PI64  = signed __int64*;
-	using PCI8  = const signed __int8*;
-	using PCI16 = const signed __int16*;
-	using PCI32 = const signed __int32*;
-	using PCI64 = const signed __int64*;
+	using PI8   = I8*;
+	using PI16  = I16*;
+	using PI32  = I32*;
+	using PI64  = I64*;
+	using PCI8  = CI8*;
+	using PCI16 = CI16*;
+	using PCI32 = CI32*;
+	using PCI64 = CI64*;
 	
 	/// Floating-point pointer types
-	using PF32  = float*;
-	using PF64  = double*;
-	using PCF32 = const float*;
-	using PCF64 = const double*;
+	using PF32  = F32*;
+	using PF64  = F64*;
+	using PCF32 = CF32*;
+	using PCF64 = CF64*;
 }
 
 #endif
