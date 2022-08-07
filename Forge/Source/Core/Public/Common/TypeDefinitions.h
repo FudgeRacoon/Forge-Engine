c3168029505749e3011229bb6fadec5bfb774b11
FudgeRacoon <FudgeRacoon@gmail.com>
2022-08-07 20:36:13 +0200

    Added type compile-time type size checks

--- Forge/Source/Core/Public/Common/TypeDefinitions.h
+++ Forge/Source/Core/Public/Common/TypeDefinitions.h
@@ -2,29 +2,26 @@
 #define TYPE_DEFINITIONS_H
 
 #include "Compiler.h"
+#include "PreprocessorUtilities.h"
 
 namespace Forge
 {
-	/// Misc types
 	using VOID = void;
 
-	/// Char types
 	using CHAR = char;
 	using CCHAR = const char;
 
-	/// Floating-point types
 	using F32  = float;
 	using F64  = double;
 	using CF32 = const float;
 	using CF64 = const double;
 
 #if defined(FORGE_COMPILER_MSVC)
-	/// Misc types
 	using SIZE  = unsigned __int64;
+
 	using BYTE  = unsigned __int8;
 	using CBYTE = const unsigned __int8;
 
-	/// Unsigned integer types
 	using U8   = unsigned __int8;
 	using U16  = unsigned __int16;
 	using U32  = unsigned __int32;
@@ -34,7 +31,6 @@ namespace Forge
 	using CU32 = const unsigned __int32;
 	using CU64 = const unsigned __int64;
 
-	/// Signed integer types
 	using I8   = signed __int8;
 	using I16  = signed __int16;
 	using I32  = signed __int32;
@@ -46,12 +42,11 @@ namespace Forge
 #elif
 	#include<stdint>
 
-	/// Misc types
 	using SIZE  = std::uint64_t;
+
 	using BYTE  = std::uint8_t;
 	using CBYTE = const std::uint8_t;
 
-	/// Unsigned integer types
 	using U8   = std::uint8_t;
 	using U16  = std::uint16_t;
 	using U32  = std::uint32_t;
@@ -61,7 +56,6 @@ namespace Forge
 	using CU32 = const std::uint32_t;
 	using CU64 = const std::uint64_t;
 
-	/// Signed integer types
 	using I8   = std::int8_t;
 	using I16  = std::int16_t;
 	using I32  = std::int32_t;
@@ -72,17 +66,15 @@ namespace Forge
 	using CI64 = const std::int64_t;
 #endif
 
-	/// Misc pointer types
 	using PVOID  = VOID*;
 	using PCVOID = const VOID*;
+
 	using PBYTE  = BYTE*;
 	using PCBYTE = CBYTE*;
 
-	/// Char pointer types
 	using PSTR  = CHAR*;
 	using PCSTR = CCHAR*;
 
-	/// Unsinged integer pointer types
 	using PU8   = U8*;
 	using PU16  = U16*;
 	using PU32  = U32*;
@@ -92,7 +84,6 @@ namespace Forge
 	using PCU32 = CU32*;
 	using PCU64 = CU64*;
 
-	/// Singed integer pointer types
 	using PI8   = I8*;
 	using PI16  = I16*;
 	using PI32  = I32*;
@@ -102,11 +93,25 @@ namespace Forge
 	using PCI32 = CI32*;
 	using PCI64 = CI64*;
 	
-	/// Floating-point pointer types
 	using PF32  = F32*;
 	using PF64  = F64*;
 	using PCF32 = CF32*;
 	using PCF64 = CF64*;
+
+	FORGE_STATIC_ASSERT(sizeof(CHAR) == 1)
+
+	FORGE_STATIC_ASSERT(sizeof(U8)  == 1)
+	FORGE_STATIC_ASSERT(sizeof(U16) == 2)
+	FORGE_STATIC_ASSERT(sizeof(U32) == 4)
+	FORGE_STATIC_ASSERT(sizeof(U64) == 8)
+
+	FORGE_STATIC_ASSERT(sizeof(I8)  == 1)
+	FORGE_STATIC_ASSERT(sizeof(I16) == 2)
+	FORGE_STATIC_ASSERT(sizeof(I32) == 4)
+	FORGE_STATIC_ASSERT(sizeof(I64) == 8)
+
+	FORGE_STATIC_ASSERT(sizeof(F32) == 4)
+	FORGE_STATIC_ASSERT(sizeof(F64) == 8)
 }
 
 #endif
