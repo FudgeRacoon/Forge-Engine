c4cbb857ec4fc90e5f43c90e308da84654009fd5
FudgeRacoon <FudgeRacoon@gmail.com>
2022-08-07 20:35:41 +0200

    Added assertions and static assertions

--- Forge/Source/Core/Public/Common/PreprocessorUtilities.h
+++ Forge/Source/Core/Public/Common/PreprocessorUtilities.h
@@ -1,6 +1,10 @@
 #ifndef PREPROCESSOR_UTILITIES_H
 #define PREPROCESSOR_UTILITIES_H
 
+#include <cstdio>
+
+#include "Compiler.h"
+
 #define FORGE_INVALID_INDEX (-1)
 
 #define FORGE_BIT(x) (1 << x)
@@ -8,13 +12,42 @@
 #define FORGE_SAFE_DELETE_UNIT(mem) if(mem != nullptr) {delete mem; mem = nullptr;}
 #define FORGE_SAFE_DELETE_BLOCK(mem) if(mem != nullptr) {delete[] mem; mem = nullptr;}
 
-#define FORGE_STRINGIZE(s) #s
-#define FORGE_CONCATENATE(s1, s2) s1##s2
-#define FORGE_CONCATENATE_VARIADIC(s, ...) s##__VA_ARGS__
-#define FORGE_CONCATENATE_COMMA_VARIADIC(s, ...) s, ##__VA_ARGS__
+#define _FORGE_STRINGIZE(s) #s
+#define  FORGE_STRINGIZE(s) _FORGE_STRINGIZE(s)
+
+#define _FORGE_CONCATENATE(s1, s2) s1 ## s2
+#define  FORGE_CONCATENATE(s1, s2) _FORGE_CONCATENATE(s1, s2)
+
+#define _FORGE_CONCATENATE_VARIADIC(s, ...) s##__VA_ARGS__
+#define  FORGE_CONCATENATE_VARIADIC(s, ...) _FORGE_CONCATENATE_VARIADIC(s, ...)
+
+#define _FORGE_CONCATENATE_COMMA_VARIADIC(s, ...) s,##__VA_ARGS__
+#define  FORGE_CONCATENATE_COMMA_VARIADIC(s, ...) _FORGE_CONCATENATE_COMMA_VARIADIC(s, ...)
 
 #define FORGE_BYTESWAP_16_UNSIGNED(x) ((((x) >> 8) & 0x00ff) + (((x) << 8) & 0xff00))
 #define FORGE_BYTESWAP_32_UNSIGNED(x) (((x) >> 24) + (((x) >> 8) & 0x00ff00) + (((x) << 8) & 0xff0000) + ((x) << 24))
+
+#if defined(FORGE_DEBUG)
+	#define FORGE_ASSERT(__EXPR__)                   \
+		if(__EXPR__) {}                              \
+		else                                         \
+		{                                            \
+			fprintf(stderr, "File: %s ", __FILE__);  \
+			fprintf(stderr, "Line: %d ", __LINE__);  \
+			fprintf(stderr, "Expr: %s ", #__EXPR__); \
+			FORGE_DEBUG_BREAK                        \
+		}                                            \
+
+	#define FORGE_STATIC_ASSERT(__EXPR__)            \
+		enum                                         \
+		{                                            \
+			FORGE_CONCATENATE(assert_fail, __LINE__) \
+				= 1 / (int) (!!(__EXPR__))           \
+		};
+#elif defined(FORGE_NDEBUG)
+	#define FORGE_ASSERT(__EXPR__)
+	#define FORGE_STATIC_ASSERT(__EXPR__)
+#endif
 
 #define FORGE_CLASS_NONCOPYABLE(TypeName)                \
 	public: TypeName(Typename&& rhs) = delete            \
@@ -22,12 +55,12 @@
 	public: Typename& operator=(Typename&& rhs) = delete \
 	public: Typename& operator=(const Typename& rhs) = delete
 
-#define FORGE_CLASS_ALLOCATOR(TypeName)         \
-	public: void* operator new(size_t size);    \
-	public: void* operator new[](size_t size);  \
-	public: void  operator delete(void* ptr);   \
+#define FORGE_CLASS_ALLOCATOR(TypeName)        \
+	public: void* operator new(size_t size);   \
+	public: void* operator new[](size_t size); \
+	public: void  operator delete(void* ptr);  \
 	public: void  operator delete[](void* ptr)
 	
-#define FORGE_FILE_LINE_LITERAL "[" __FILE__ "][" __LINE__ "]:"
+#define FORGE_FILE_LINE_LITERAL "[" __FILE__ "][" __LINE__ "]: "
 
-#endif
+#endif // PREPROCESSOR_UTILITIES_H
