3eb6d8b933b324ac65e7463ff42ee1d8158ab3dd
FudgeRacoon <FudgeRacoon@gmail.com>
2022-08-20 17:05:53 +0200

    Added memory and pointer utilities for memory management

--- Forge/Source/Core/Public/Memory/MemoryUtilities.h
+++ Forge/Source/Core/Public/Memory/MemoryUtilities.h
@@ -1,1 +1,105 @@
-#pragma once
+#ifndef MEMORY_UTILITIES_H
+#define MEMORY_UTILITIES_H
+
+#include "Core/Public/Common/Compiler.h"
+#include "Core/Public/Common/TypeDefinitions.h"
+
+namespace Forge {
+	namespace Memory
+	{
+		/// @brief Aligns address to an alignment boundry and retrieve the first aligned
+		/// address after the address passed.
+		/// @param[in] address The address to align.
+		/// @param[in] alignment The Alignment requirment of the memory, must be power of two.
+		/// @returns The address of the aligned address in integral form.
+		FORGE_FORCE_INLINE PVOID AlignAddressUpward(PVOID address, BYTE alignment)
+		{
+			return (PVOID)((reinterpret_cast<INTPTR>(address) + (alignment - 1)) & ~(alignment - 1));
+		}
+
+		/// @brief Aligns address to an alignment boundry and retrieve the first aligned
+		/// address before the address passed.
+		/// @param[in] address The address to align.
+		/// @param[in] alignment The Alignment requirment of the memory, must be power of two.
+		/// @returns The address of the aligned address in integral form.
+		FORGE_FORCE_INLINE PVOID AlignAddressBackward(PVOID address, BYTE alignment)
+		{
+			return (PVOID)((reinterpret_cast<INTPTR>(address) & ~(alignment - 1)));
+		}
+
+		/// @brief Calculates how many bytes are need to align the address upwards.
+		/// @param[in] address The address to align.
+		/// @param[in] alignment The Alignment requirment of the memory, must be power of two.
+		/// @returns The number of bytes to align address.
+		FORGE_FORCE_INLINE BYTE AlignAddressUpwardAdjustment(PVOID address, BYTE alignment)
+		{
+			BYTE adjustment = alignment - (reinterpret_cast<INTPTR>(address) & (alignment - 1));
+			return adjustment != alignment ? adjustment : 0;
+		}
+
+		/// @brief Calculates how many bytes are need to align the address backwards.
+		/// @param[in] address The address to align.
+		/// @param[in] alignment The Alignment requirment of the memory, must be power of two.
+		/// @returns The number of bytes to align address.
+		FORGE_FORCE_INLINE BYTE AlignAddressBackwardAdjustment(PVOID address, BYTE alignment)
+		{
+			BYTE adjustment = (reinterpret_cast<INTPTR>(address) & (alignment - 1));
+			return adjustment != alignment ? adjustment : 0;
+		}
+
+		/// @brief Checks whether address is aligned to a specific alignment boundry.
+		/// @param[in] address The address to check for alignment.
+		/// @param[in] alignment The Alignment requirment of the memory, must be power of two.
+		FORGE_FORCE_INLINE BOOL IsAddressAligned(PVOID address, BYTE alignment)
+		{ 
+			return AlignAddressUpwardAdjustment(address, alignment) == 0;
+		}
+		
+		/// @brief Performs addition on a non constant void pointer.
+		/// @param[in] address The address to perform addition on.
+		/// @param[in] bytes The number of bytes to add to the address.
+		FORGE_FORCE_INLINE PVOID AddAddress(PVOID address, SIZE bytes)
+		{
+			return (PVOID)(reinterpret_cast<INTPTR>(address) + bytes);
+		}
+
+		/// @brief Performs addition on a constant void pointer.
+		/// @param[in] address The address to perform addition on.
+		/// @param[in] bytes The number of bytes to add to the address.
+		FORGE_FORCE_INLINE PCVOID AddAddress(PCVOID address, SIZE bytes)
+		{
+			return (PCVOID)(reinterpret_cast<INTPTR>(address) + bytes);
+		}
+
+		/// @brief Performs subtraction on a non constant void pointer.
+		/// @param[in] address The address to perform subtraction on.
+		/// @param[in] bytes The number of bytes to subtract to the address.
+		FORGE_FORCE_INLINE PVOID SubAddress(PVOID address, SIZE bytes)
+		{
+			return (PVOID)(reinterpret_cast<INTPTR>(address) - bytes);
+		}
+
+		/// @brief Performs subtraction on a constant void pointer.
+		/// @param[in] address The address to perform subtraction on.
+		/// @param[in] bytes The number of bytes to subtract to the address.
+		FORGE_FORCE_INLINE PCVOID SubAddress(PCVOID address, SIZE bytes)
+		{
+			return (PCVOID)(reinterpret_cast<INTPTR>(address) - bytes);
+		}
+		
+		/// @brief Checks whether the marker address is within bounds of the allocated memory.
+		/// @param[in] start_address The starting address of the memory pool.
+		/// @param[in] marker_address The address to bound check.
+		/// @param[in] total_size The total size of the memory pool.
+		FORGE_FORCE_INLINE BOOL WithinAddressBounds(PVOID start_address, PVOID marker_address, SIZE total_size)
+		{
+			if (reinterpret_cast<INTPTR>(start_address) > reinterpret_cast<INTPTR>(marker_address) ||
+				reinterpret_cast<INTPTR>(start_address) + total_size < reinterpret_cast<INTPTR>(marker_address))
+				return false;
+			else
+				return true;
+		}
+	}
+}
+
+#endif // MEMORY_UTILITIES_H
