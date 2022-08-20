3b9454ea358893172015cfeeff0ff2600abdff86
FudgeRacoon <FudgeRacoon@gmail.com>
2022-08-20 17:06:41 +0200

    Implemented linear allocator

--- Forge/Source/Core/Public/Memory/LinearAllocator.h
+++ Forge/Source/Core/Public/Memory/LinearAllocator.h
@@ -1,1 +1,33 @@
-#pragma once
+#ifndef LINEAR_ALLOCATOR_H
+#define LINEAR_ALLOCATOR_H
+
+#include <stdlib.h>
+
+#include "MemoryUtilities.h"
+#include "AbstractAllocator.h"
+
+#include "Core/Public/Common/Compiler.h"
+#include "Core/Public/Common/TypeDefinitions.h"
+
+namespace Forge {
+	namespace Memory
+	{
+		class FORGE_API LinearAllocator : public AbstractAllocator
+		{
+		private:
+			PVOID m_offset_ptr;
+
+		public:
+			LinearAllocator(SIZE total_size);
+			LinearAllocator(PVOID start, SIZE total_size);
+		   ~LinearAllocator();
+
+		public:
+			PVOID Allocate(SIZE size, BYTE alignment = 4) override;
+			VOID  Deallocate(PVOID address) override;
+			VOID  Clear() override;
+		};
+	}
+}
+
+#endif // LINEAR_ALLOCATOR_H
